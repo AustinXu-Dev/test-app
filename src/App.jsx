5fe735c66ad97d8756ce745b2d5418ce9c9e9e3b
Pyae Phyo Aung (Austin) <76926850+AustinXu-Dev@users.noreply.github.com>
2026-03-12 23:33:30 +0700

    fixx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.jsx</code> and save to reload.
+          Edit <code>src/App.jsx</code> and save tdfdfdfdfo reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Fixing
         </a>
       </header>
     </div>
